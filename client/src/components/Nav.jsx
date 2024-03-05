4f83b9be924d8b8c4978bc63b8dba07a8e9f48d5
Vignesh <vigneshpamu2002@gmail.com>
2024-03-06 02:46:34 +0530

    sfs

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -39,7 +39,7 @@ const Nav = ({ currentUser, currentUrl }) => {
               alt="Flowbite Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              FANTASI
+              FANTASIES
             </span>
           </a>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
